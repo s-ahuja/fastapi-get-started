dc698fd0d6bcc8ca150def5003f0ea8047c838d1
Saurabh Ahuja <>
2021-09-05 16:57:24 +0530

    played with alembic model changes

--- bookstore/models/item.py
+++ bookstore/models/item.py
@@ -6,7 +6,8 @@ from db import Base
 class ItemModel(Base):
     __tablename__ = 'items'
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(length=20))
-    description = Column(String(length=200))
+    title = Column(String(length=20), nullable=False)
+    description = Column(String(length=200), nullable=False)
+    # tempcol = Column(String(length=20), nullable=False)
     # user_id = Column(Integer, ForeignKey('users.id'))
     # creator = relationship("User", back_populates="blogs")
